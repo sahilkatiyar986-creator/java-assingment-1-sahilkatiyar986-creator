c6a5681c818b4e1e4a1939c54a7efc2f80cd06e3
sahilkatiyar986-creator <sahilkatiyar986@gmail.com>
2026-04-07 14:31:07 +0530

    Implement diamond pattern printing in Main.java
    Added code to print a diamond pattern using asterisks.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -20,6 +20,32 @@ public class Main {
         //  *   *
         //   * *
         //    *
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * (i - 1) - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * (i - 1) - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
