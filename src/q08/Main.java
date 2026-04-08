877fd0990103e1ca4fb8b7ff0a5231e3326e781a
sahilkatiyar986-creator <sahilkatiyar986@gmail.com>
2026-04-08 11:16:13 +0530

    question 8 solved

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -4,28 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        // TODO: Implement a stack using an array (no Java Stack class)
-        //       Process n operations:
-        //         PUSH x  → push integer x
-        //         POP     → pop top, print it; if empty print "EMPTY"
-        //         PEEK    → print top without removing; if empty print "EMPTY"
-        //         SIZE    → print current number of elements
-        //
-        // Input:
-        // 6
-        // PUSH 10
-        // PUSH 20
-        // PEEK
-        // POP
-        // POP
-        // POP
-        //
-        // Output:
-        // 20
-        // 20
-        // 10
-        // EMPTY
-
+        sc.nextLine(); 
+        
+        // Stack implementation using array
+        int[] stack = new int[1000]; 
+        int top = -1; 
+        
+        for (int i = 0; i < n; i++) {
+            String operation = sc.next();
+            
+            if (operation.equals("PUSH")) {
+                int x = sc.nextInt();
+                stack[++top] = x; 
+            }
+            else if (operation.equals("POP")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]);
+                    top--; 
+                }
+            }
+            else if (operation.equals("PEEK")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]);
+                }
+            }
+            else if (operation.equals("SIZE")) {
+                System.out.println(top + 1); 
+            }
+            
+            sc.nextLine(); 
+        }
+        
+        sc.close();
     }
 }
