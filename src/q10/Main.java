31eafaabdc0341aaff2a3d568c98393a1f0df989
sahilkatiyar986-creator <sahilkatiyar986@gmail.com>
2026-04-08 11:18:01 +0530

    question 10 solved

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -1,21 +1,43 @@
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // TODO: Evaluate a postfix (Reverse Polish Notation) expression
-        //       Operands are non-negative integers, operators are: + - * /
-        //       Use a stack to evaluate
-        //       Input: single line, tokens separated by spaces
-        //       Output: integer result
-        //
-        // Input: 5 1 2 + 4 * + 3 -
-        // Output: 14
-        //
-        // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
-
+        
+        // Read entire line of postfix expression
         String line = sc.nextLine();
-
+        String[] tokens = line.split(" ");
+        
+        // Use stack to evaluate postfix expression
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || 
+                token.equals("*") || token.equals("/")) {
+                // Operator: pop two operands, apply operation, push result
+                int b = stack.pop();
+                int a = stack.pop();
+                
+                int result;
+                switch (token) {
+                    case "+": result = a + b; break;
+                    case "-": result = a - b; break;
+                    case "*": result = a * b; break;
+                    case "/": result = a / b; break; // integer division
+                    default: continue;
+                }
+                
+                stack.push(result);
+            } else {
+                // Operand: push to stack
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        // Final result is on top of stack
+        System.out.println(stack.pop());
+        
+        sc.close();
     }
 }
