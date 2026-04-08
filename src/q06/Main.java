652930d6f254b139db12e687b67364000845a828
sahilkatiyar986-creator <sahilkatiyar986@gmail.com>
2026-04-08 11:14:37 +0530

    question 6 solved

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -5,11 +5,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
-
+        
+        if (n > 0) {
+            System.out.print("0");
+        }
+        if (n > 1) {
+            System.out.print(" 1");
+        }
+        
+        int a = 0, b = 1;
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+        
+        System.out.println();
+        sc.close();
     }
 }
