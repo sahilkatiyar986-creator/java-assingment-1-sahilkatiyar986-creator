af3e2ef559ff58cb2e180b5151751800e9b4110f
sahilkatiyar986-creator <sahilkatiyar986@gmail.com>
2026-04-08 08:15:30 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -5,7 +5,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
+      import java.util.*;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * (i - 1) - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * (i - 1) - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}  // TODO: Print a hollow diamond pattern of half-height n using '*'
         //
         // Rules:
         //   - Only border cells have '*', inside is spaces
