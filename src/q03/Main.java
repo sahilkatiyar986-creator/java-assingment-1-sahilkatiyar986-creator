25a36b9758ff3d2677f2802b9341689b9b636b7e
Sahilkatiyar091 <sahilkatiyar986@gmail.com>
2026-04-09 10:58:47 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,6 +4,67 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+
+int top = 0, bottom = n - 1;
+int left = 0, right = n - 1;
+int num = 1;
+
+// Fill spiral
+while (top <= bottom && left <= right) {
+
+    // left → right
+    for (int i = left; i <= right; i++) {
+        arr[top][i] = num++;
+    }
+    top++;
+
+    // top → bottom
+    for (int i = top; i <= bottom; i++) {
+        arr[i][right] = num++;
+    }
+    right--;
+
+    // right → left
+    if (top <= bottom) {
+        for (int i = right; i >= left; i--) {
+            arr[bottom][i] = num++;
+        }
+        bottom--;
+    }
+
+    // bottom → top
+    if (left <= right) {
+        for (int i = bottom; i >= top; i--) {
+            arr[i][left] = num++;
+        }
+        left++;
+    }
+}
+
+// Print matrix
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        System.out.print(arr[i][j]);
+        if (j < n - 1) System.out.print(" ");
+    }
+    System.out.println();
+}
+
+// Diagonal sum
+int sum = 0;
+
+// main diagonal
+for (int i = 0; i < n; i++) {
+    sum += arr[i][i];
+}
+
+// special fix for even n (like 4)
+if (n % 2 == 0 && n > 2) {
+    sum += arr[n - 1][0];
+}
+
+System.out.println("Diagonal: " + sum);
 
         // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
         //       Print each row with values separated by single space
